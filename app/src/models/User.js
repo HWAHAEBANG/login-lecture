c867ee67bd04c4126c5d4bb8c5c75f83bea444d7
화해방 <bcl0206@naver.com>
2023-02-27 19:16:25 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -24,11 +24,15 @@ class User {
     return { success: false, msg: "존재하지 않는 아이디 입니다." };
   }
 
-  register() {
+  async register() {
     const client = this.body;
-    // UserStorage에 save라는 메서드를 호출해서 데이터가 저장되도록 해주겠음.
-    const response = UserStorage.save(client); // 저장될 데이터 (this.body)를 스토리지로 던져주기
-    return response; // 리스폰스로 받아서 반환해준디
+    try {
+      // UserStorage에 save라는 메서드를 호출해서 데이터가 저장되도록 해주겠음.
+      const response = await UserStorage.save(client); // 저장될 데이터 (this.body)를 스토리지로 던져주기
+      return response; // 리스폰스로 받아서 반환해준디
+    } catch (err) {
+      return { success: false, msg: err };
+    }
   }
 }
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -18,15 +18,28 @@ class UserStorage {
     return userInfo;
   }
 
-  static getUsers(...fields) {
-    // const users = this.#users;
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) return users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
       }
       return newUsers;
     }, {});
+
     return newUsers;
+  }
+
+  static getUsers(isAll, ...fields) {
+    // const users = this.#users;
+    //fs모듈의 readfile을 통해서 파일을 읽을 것임
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUsers(data, isAll, fields);
+      }) // fetch와 같이 프로미스를 반환하기 때문에 then이라는 메소드에 접근을 할수 있게된다.
+      .catch((err) => console.error(err)); // 그리고 오류도 잡아낼 수 있지.
   }
 
   static getUserInfo(id) {
@@ -40,7 +53,8 @@ class UserStorage {
       .catch((err) => console.error(err)); // 그리고 오류도 잡아낼 수 있지.
   }
 
-  static save(userInfo) {
+  static async save(userInfo) {
+    // 아래 코드는 이제 필요 없다.
     // 클라이언트에서 전달한 데이터를 저장해주는 함수"만" 만들었음.
     // const users = this.#users;
     // users.id.push(userInfo.id);
@@ -48,6 +62,19 @@ class UserStorage {
     // users.psword.push(userInfo.psword);
     // return { success: true };
     // 받아오는 데이터가 user의 정보이기 때문에 userInfo로 받아준다.
+
+    // 일단 이미 만들어 놓은 메서드를 사용해서 다 불러온다
+    const users = await this.getUsers(true); // = "id", "psword", "name"
+    // console.log(users);
+    if (users.id.includes(userInfo.id)) {
+      throw "이미 존재하는 아이디입니다.";
+    }
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.psword.push(userInfo.psword);
+    // 파일에 데이터를 쓰기위한 메서드(?) (저장할 경로, 저장할 데이터)
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+    return { success: true };
   }
 }
 
